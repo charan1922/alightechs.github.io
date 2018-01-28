c2e3a05fe7aca8955bd6c6ce4bc35f1e98ad5f7d
Charan Teja <charaneie22@gmail.com>
2018-01-28 14:33:01 +0530

    added pages (#1)
    added basic pages

---
+++ src/pages/connect.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Connect extends Component {
+  render() {
+    return (
+        <div className="connect-container">
+        <p>Connect</p>
+      </div>
+    )
+  }
+}
---
+++ src/pages/courses.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Courses extends Component {
+  render() {
+    return (
+      <div className="courses-container">
+        <p>Courses</p>
+      </div>
+    );
+  }
+}
